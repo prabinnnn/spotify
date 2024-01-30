368d4a43d7bb5c5b336165f09c380fafec14886a
prabinnnn <prabinbhandari120@gmail.com>
2024-01-30 21:54:45 +0545

    add index

--- sofityfy/index.js
+++ sofityfy/index.js
@@ -1,7 +1,9 @@
 console.log("welcome to spotify");
 let songindex = 0;
-let audioelement = new audio("1.mp3");
+let audioelement = new Audio("1.mp3");
 let masterplay = document.getElementById("masterPlay");
+let myprogessbar = document.getElementById("myprogessbar");
+let gif = document.getElementById("gif");
 let song = [
   {
     songname: "salma isqh",
@@ -59,4 +61,17 @@ let song = [
     coverpage: "1.jpg",
   },
 ];
-document.addEventListener("time");
+masterplay.addEventListener("click", () => {
+  if (audioelement.paused || audioelement.currentTime <= 0) {
+    audioelement.play();
+    masterplay.classList.remove("fa-play-circle");
+    masterplay.classList.add("fa-pause-play");
+  } else {
+    audioelement.pause();
+    masterplay.classList.remove("fa-pause-play");
+    masterplay.classList.add("fa-play-circle");
+  }
+});
+document.addEventListener("timeupdate", () => {
+  console.log("timeupdate");
+});

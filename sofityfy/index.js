f556043d550198fc8b88164dc271a77d0b5b5b73
prabinnnn <prabinbhandari120@gmail.com>
2024-01-30 12:02:47 +0545

    add index

--- sofityfy/index.js
+++ sofityfy/index.js
@@ -1,2 +1,62 @@
 console.log("welcome to spotify");
-let audioelement = new audio("");
+let songindex = 0;
+let audioelement = new audio("1.mp3");
+let masterplay = document.getElementById("masterPlay");
+let song = [
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+  {
+    songname: "salma isqh",
+    filepath: "music/1.mp3",
+    coverpage: "1.jpg",
+  },
+];
+document.addEventListener("time");
